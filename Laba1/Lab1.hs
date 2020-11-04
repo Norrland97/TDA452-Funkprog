497edb5f65235a1b6ec852dbc85fbeda32a9ef4e
Aline <aline.eikeland@gmail.com>
2020-11-04 18:20:17 +0100

    added test commenting

--- Laba1/Lab1.hs
+++ Laba1/Lab1.hs
@@ -62,17 +62,18 @@ powerTest' ns ks | length ns == 0 = []
                  | otherwise      = prop_powers (head ns) (head ks) : powerTest' (tail ns) (tail ks)
 
 nTestValues :: [Integer]
-nTestValues = [3,6,0,1,5,-2,10000000,4,2]
+nTestValues = [3,0,6,0,1,5,-2,10000000,4,2]
 
 kTestValues :: [Integer]
-kTestValues = [6,2,7,10,0,4,4,20000,-5]
+kTestValues = [6,0,0,7,10,0,4,4,20000,-5]
 
 {- 
 
 <Describe your test cases here>
 We chose the values to see that some basic values would work, and then to see that negative values and big numbers works.
-As the last test value we chose to have a negative k-value, tihs gives an error and that is expected, we wanted to catch the error but did not know how so we left it as an exception/error.
-We did tru a k- value of 200000 bu that took a bit too long to run.
+As the last test value we chose to have a negative k-value, tihs gives an error and that is expected. Catchings errors is not praxis in Haskell, so we instead made sure to put the error inducing value last in the list.
+20 000 is chosen as the largest exponent value that took a reasonable amount of time to run.
+We made sure to test cases with a base 0, an exponent 0 as well as both base and exponent 0.
 
  -}
 

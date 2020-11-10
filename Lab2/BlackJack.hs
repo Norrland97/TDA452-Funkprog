3aac467fea7ab395d185120ef83d4f7c6dda177a
Aline <aline.eikeland@gmail.com>
2020-11-10 17:03:08 +0100

    bg

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -1,6 +1,10 @@
 module BlackJack where
 import Cards
 import RunGame
+import Test.QuickCheck
+
+hand2 = (Add (Card (Numeric 2) Hearts)
+                        (Add (Card Jack Spades) Empty))
 
 sizeSteps :: [Integer]
 sizeSteps = [ size hand2
@@ -11,4 +15,17 @@ sizeSteps = [ size hand2
             , 1 + 1 + 0
             , 2]
 
-            
+
+--display :: Hand -> String
+--display = 
+
+
+displayCard :: Card -> String
+displayCard (Card rank suit) = show rank ++ " of " ++  show suit
+
+
+
+
+
+--displayCard c = show c.Rank ++ "of" ++ show c.Suit       
+        

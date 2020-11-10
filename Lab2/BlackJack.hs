7d358ef6a7d80c05f1d9a3f1437866a5e1e0b7cd
Aline <aline.eikeland@gmail.com>
2020-11-10 16:13:53 +0100

    we

---
+++ Lab2/BlackJack.hs
@@ -0,0 +1,14 @@
+module BlackJack where
+import Cards
+import RunGame
+
+sizeSteps :: [Integer]
+sizeSteps = [ size hand2
+            , size (Add (Card (Numeric 2) Hearts)
+                        (Add (Card Jack Spades) Empty))
+            , 1 + size (Add (Card Jack Spades) Empty)
+            , 1 + 1 + size (Empty)
+            , 1 + 1 + 0
+            , 2]
+
+            

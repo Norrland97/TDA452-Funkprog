a376122a15a68bda150759fb592fc320a20a10ea
Aline <aline.eikeland@gmail.com>
2020-11-11 11:41:24 +0100

    lab 2 finished

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -3,9 +3,16 @@ import Cards
 import RunGame
 import Test.QuickCheck
 
+-- These hands are used for testruns
 hand2 = (Add (Card (Numeric 2) Hearts)
-                        (Add (Card Jack Spades) Empty))
+                       (Add (Card Jack Spades) Empty))  -- No aces, hand value 13
+hand3 = (Add (Card (Numeric 2) Hearts)
+                       (Add (Card Jack Spades) 
+                                      (Add (Card Ace Spades) Empty))) -- One ace which has value 1, hand value 14
+hand4 = (Add (Card (Numeric 3) Spades) 
+                       (Add (Card Ace Spades) Empty))         --One ace which has value 11, hand value 14                           
 
+--shows the steps taken by the 'size' function
 sizeSteps :: [Integer]
 sizeSteps = [ size hand2
             , size (Add (Card (Numeric 2) Hearts)
@@ -15,17 +22,53 @@ sizeSteps = [ size hand2
             , 1 + 1 + 0
             , 2]
 
+-- converts the hand to a string, cards seperated with a blank space.
+display :: Hand -> String
+display Empty = ""
+display (Add card hand) = displayCard card ++ " " ++ display hand
 
---display :: Hand -> String
---display = 
-
-
+-- converts a card to a string
 displayCard :: Card -> String
-displayCard (Card rank suit) = show rank ++ " of " ++  show suit
+displayCard (Card rank suit) | rank == Jack 
+                            || rank == Queen 
+                            || rank == King 
+                            || rank == Ace 
+                                          = show rank ++ " of " ++  show suit
+                             | otherwise  = numToString rank ++ " of " ++  show suit                        
+        where numToString (Numeric value) = show value
+
+-- calculates the value of a hand, checs if it's above 21 if so recalculates with aces as value 1 
+value :: Hand -> Integer
+value Empty = 0
+value hand = valueCount highAce hand
+        where highAce = valueCount True hand <= 21
+
+-- recursively checks the value of a hand. Argument boolean sets the value for the aces of the hand.
+valueCount :: Bool -> Hand -> Integer
+valueCount bool Empty                       = 0
+valueCount bool (Add (Card rank suit) hand) = (valueRank bool rank) + (valueCount bool hand)
+
+-- checks the value od a card. Argument boolean sets ace value depending
+valueRank :: Bool -> Rank -> Integer
+valueRank _ Jack       = 11
+valueRank _ Queen      = 12
+valueRank _ King       = 13
+valueRank True Ace     = initialValue
+        where initialValue = 11
+valueRank False Ace    = newAce
+        where newAce = 1
+valueRank _ rank       = numToInt rank
+        where numToInt (Numeric i) = i
 
 
+-- checks whether a hand passes 21 in value
+gameOver :: Hand -> Bool
+gameOver hand = value hand > 21 
+                   
+-- returns the winner of argument two hands, where the first is the guest player and the second the bank player
+winner :: Hand -> Hand -> Player
+winner guest bank | gameOver guest               = Bank
+                  | gameOver bank                = Guest
+                  | (value guest) > (value bank) = Guest
+                  | otherwise                    = Bank
 
-
-
---displayCard c = show c.Rank ++ "of" ++ show c.Suit       
-        

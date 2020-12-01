98ff9a9cc4e258054f158c34da84bb04770f155b
Viktor Fredholm <vikfre@student.chalmers.se>
2020-12-01 10:20:02 +0100

    Return on lab 3A

--- Lab3/Sudoku.hs
+++ Lab3/Sudoku.hs
@@ -1,7 +1,8 @@
 module Sudoku where
 
 import Test.QuickCheck
-import Data.List ( genericLength, nub,transpose, splitAt )
+import Data.List ( genericLength, nub,transpose, splitAt)
+import Data.Maybe (catMaybes)
 
 ------------------------------------------------------------------------------
 
@@ -50,11 +51,21 @@ example' =
     n = Nothing
     j = Just
 
+exampleRow :: [Maybe Int]
 exampleRow =
     [j 3,j 6,j 8,j 8,j 7,j 1,j 2,j 8,j 6]
   where
     n = Nothing
     j = Just
+
+exampleRow' :: [Maybe Int]
+exampleRow' =
+    [n  ,j 6,j 7,j 3,n  ,j 1,n  ,j 8,j 5]
+  where
+    n = Nothing
+    j = Just   
+    
+     
 -- * A1
 
 -- | allBlankSudoku is a sudoku with just blanks
@@ -71,19 +82,20 @@ allBlankSudoku = Sudoku allBlankTable
 -- | isSudoku sud checks if sud is really a valid representation of a sudoku
 -- puzzle
 isSudoku :: Sudoku -> Bool
-isSudoku (Sudoku rs) = nineLong rs && isRows rs
-    where isRows rs | null rs   = True
-                    | otherwise = nineLong (head rs) && isRows (tail rs)
-          nineLong row = length row == 9
+isSudoku (Sudoku rs) = nineLong rs && all nineLong rs && all numRange rs
+        where nineLong row = length row == 9
+              numRange r = all oneToNine (catMaybes r)
+              oneToNine i = i >= 1 && i <= 9
+              
 
 -- * A3
 
 -- | isFilled sud checks if sud is completely filled in,
 -- i.e. there are no blanks
 isFilled :: Sudoku -> Bool
-isFilled (Sudoku (r:[])) = isRowFilled r
-isFilled (Sudoku (r:rs)) = isRowFilled r && isFilled (Sudoku rs)
+isFilled (Sudoku rs) = all isRowFilled rs
 
+-- a helper function which check if a row is completely filled in
 isRowFilled :: [Maybe a] -> Bool
 isRowFilled = all notNull
        where notNull a = not (null a)
@@ -99,16 +111,15 @@ isRowFilled = all notNull
 -- | printSudoku sud prints a nice representation of the sudoku sud on
 -- the screen
 printSudoku :: Sudoku -> IO ()
-printSudoku (Sudoku (r:[])) = do printRow r
-printSudoku (Sudoku (r:rs)) = do printRow r 
-                                 printSudoku (Sudoku rs)
+printSudoku (Sudoku [])     =  return ()
+printSudoku (Sudoku rs) = do putStr (unlines (rowsToString rs))
 
-printRow :: Row -> IO ()
-printRow (x:[]) = do putStr (showCell x)
-                     putStrLn ""
-printRow (x:xs) = do putStr (showCell x)
-                     printRow xs
+-- Converts a list of rows to a list of strings where the string represents a row        
+rowsToString :: [Row] -> [String]
+rowsToString = map rowToCell
+    where rowToCell r = concatMap showCell r
 
+-- helper function which converts a Cell to a String representation
 showCell :: Cell -> String
 showCell Nothing     = "."
 showCell (Just num) = show num
@@ -124,13 +135,16 @@ readSudoku file = do
         if isSudoku sud
             then return sud
             else error "readSudoku: Not a soduku!"
-
-
+            
+-- readSudoku helper reads String to Row
 readRow :: String -> Row
-readRow (b:aStr) | b == '.'  = Nothing : aA aStr
-                 | otherwise = Just (read [b]) : aA aStr
-    where aA [] = []
-          aA _  = readRow aStr
+readRow (c:aStr) | null aStr = [readCell c]
+                 | otherwise = readCell c : readRow aStr
+
+-- converts a char to a Cell
+readCell :: Char -> Cell
+readCell c | c == '.'   = Nothing
+           | otherwise  = Just (read [c])
 
 ------------------------------------------------------------------------------
 
@@ -139,7 +153,7 @@ readRow (b:aStr) | b == '.'  = Nothing : aA aStr
 -- | cell generates an arbitrary cell in a Sudoku
 cell :: Gen (Cell)
 cell = frequency [(9,no),(1,num)]
-    where no  = elements [Nothing]
+    where no  = return Nothing
           num = elements (map Just [1..9])
 
 
@@ -154,7 +168,7 @@ instance Arbitrary Sudoku where
  -- hint: get to know the QuickCheck function vectorOf
  
 -- * C3
-
+-- test function for isSudoku
 prop_Sudoku :: Sudoku -> Bool
 prop_Sudoku = isSudoku
   -- hint: this definition is simple!
@@ -165,45 +179,48 @@ type Block = [Cell] -- a Row is also a Cell
 
 
 -- * D1
-
+-- checks if a block is correctly filled in
 isOkayBlock :: Block -> Bool
-isOkayBlock block = length (nub block) == 9
+isOkayBlock block = length (catMaybes (nub block)) == length (catMaybes block) 
 
 
 -- * D2
-
+-- converts a Sudoku to 27 Blocks, the task areas of the Sudoku
 blocks :: Sudoku -> [Block]
 blocks sudoku = blocksCol sudoku ++ blocksRow sudoku ++ blocksBox (1,1) sudoku
 
+-- blocks helper which constructs Blocks representing the Sudoku rows
 blocksCol :: Sudoku -> [Block]
 blocksCol (Sudoku rs) = transpose rs
 
+-- blocks helper which constructs Blocks representing the Sudoku columns
 blocksRow :: Sudoku -> [Block]
 blocksRow (Sudoku rs) = rs
 
+-- blocks helper which constructs Blocks representing the Sudoku boxes
 blocksBox :: (Int, Int) -> Sudoku -> [Block]
-blocksBox (row, col) (Sudoku rows) | col == -1 = []
-                                   | otherwise = concat 
-                                                    (third col
-                                                        (transpose ( third row rows))) 
-                                                            : blocksBox (nR, nC) (Sudoku rows) 
-    where nR | col == 3 && row /= 3 = row + 1
-             | otherwise = row
-          nC | col /= 3 = col + 1
-             | row /= 3 = 1
-             | otherwise = -1
-          third metric input = drop (3*(metric-1))(take (3*metric) input)
+blocksBox (row, col) (Sudoku rows) | col == -1 = []                                                 -- col == -1 is a flag indicating that we are done traversing the sudoku
+                                   | otherwise = concat                                             -- concat flattens the 3x3 matrix to a length 9 list
+                                                    (third col                                      -- "chops" a third of the sudoku in the other direction
+                                                        (transpose ( third row rows)))              -- "chops" a third of the sudoku in the first direction. Then transposes the result from a 3*9 matrix to a 9*3 so that we can "chop higher".
+                                                            : blocksBox (nR, nC) (Sudoku rows)      -- recursively continues for the nine boxes of the sudoku
+    where nR | col == 3 && row /= 3 = row + 1                                                       -- these calculates the bounds of the next box
+             | otherwise            = row
+          nC | col /= 3             = col + 1
+             | row /= 3             = 1
+             | otherwise            = -1
+          third metric input        = drop (3*(metric-1))(take (3*metric) input)                    -- this local helper function takes a wanted prefix and then drops an unwanted prefix
+                                                                                                    -- e.g. we want the middle part: takes the first six elements, then drops the first three elements of this 
 
+      
 
-test' row col (Sudoku rows) = concat (drop (3*(col-1)) (take (3*col) (transpose ( drop (3*(col-1)) (take (3*row) rows)))))        
-
-
+-- test for blocks function
 prop_blocks_lengths :: Sudoku -> Bool
 prop_blocks_lengths sudoku = length bs == 27 && and [length x == 9 | x<-bs]
         where bs          = blocks sudoku 
 
 -- * D3
-
+-- checks if the sudoku contains blocks with repeated digits
 isOkay :: Sudoku -> Bool
 isOkay sudoku = all isOkayBlock (blocks sudoku)
 
